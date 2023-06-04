c3aefb63c3c49b8688baf49b23ad27f74baceeaf
nobody-27 <nobodypal27@gmail.com>
2023-06-04 22:13:10 +0530

    minor changes in views

--- Json_Web_Token/views.py
+++ Json_Web_Token/views.py
@@ -6,16 +6,18 @@ from rest_framework.generics import GenericAPIView
 from rest_framework import status
 from rest_framework.response import Response
 from Json_Web_Token.serializers import Student_Serializer_Json
+import logging
 # Create your views here.
 
 
 class Student_class(GenericAPIView):
     serializer_class = Student_Serializer_Json
-    authentication_classes = [JWTAuthentication]
-    permission_classes = [IsAuthenticated]
+    # authentication_classes = [JWTAuthentication]
+    # permission_classes = [IsAuthenticated]
 
     def get(self,request):
         data = Student_data.objects.all()
+        # 2
         serilizer = self.serializer_class(data,many=True)
         print(serilizer.data,"data")
         return Response({"status": "success", "data": serilizer.data, "code": status.HTTP_200_OK})
